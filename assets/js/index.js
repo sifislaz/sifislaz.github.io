845f8dadef6f5e03603448f06740728d1a73e644
Sifis Lazanakis <jlazanakis@gmail.com>
2025-03-04 18:45:17 +0200

    Back to top and favicon

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,44 @@
 let topOffset = 0;
-addEventListener('DOMContentLoaded',()=>{
+const backToTop = document.querySelector('#backToTop');
+
+addEventListener('DOMContentLoaded', () => {
+    // Define the Bootstrap tooltips
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-    for(let tooltip of tooltipTriggerList){
+    for (let tooltip of tooltipTriggerList) {
         topOffset = tooltip.offsetTop;
-        tooltip.setAttribute('data-bs-offset',`[0,${topOffset-880}]`);
+        tooltip.setAttribute('data-bs-offset', `[0,${topOffset - 880}]`);
     }
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
 
-})
+});
 
-
-addEventListener('resize',()=>{
+// Recalculate the tooltip offset on window resize
+addEventListener('resize', () => {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-    for(let tooltip of tooltipTriggerList){
+    for (let tooltip of tooltipTriggerList) {
         topOffset = tooltip.offsetTop;
-        tooltip.setAttribute('data-bs-offset',`[0,${topOffset-880}]`);
+        tooltip.setAttribute('data-bs-offset', `[0,${topOffset - 880}]`);
     }
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
 
-})
+});
+
+addEventListener('scroll', () => {
+    if (window.scrollY > 150) {
+        backToTop.style.display = 'block';
+        backToTop.style.position = "sticky";
+        backToTop.style.bottom = "5%";
+    }
+    else {
+        backToTop.style.display = 'none';
+    }
+});
+
+backToTop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
